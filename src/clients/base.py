87de4bf38de8f638609b1b36e0152fa4256341ad
Eli-ezer Reuven Ramirez Ruiz <ramirez.ruiz.eliezer.reuven@gmail.com>
2026-07-09 04:40:41 -0400

    refactor(client): convert BCChBaseClient to abstract class with core methods

--- src/clients/base.py
+++ src/clients/base.py
@@ -1,13 +1,22 @@
+from abc import ABCMeta
+from abc import abstractmethod
+from datetime import datetime, date
+
 from httpx_retries import RetryTransport, Retry
+
 from dataclasses import dataclass, field
 
 from src.parameters.builder import ParameterBuilder
 
 from src.types.auth import Credentials
+from src.types.response.model import WebServiceResponse
+from src.types.enums import Frequency
+
 from src.exceptions import InvalidsCredentialsError
 
+
 @dataclass(slots=True)
-class BCChBaseClient:
+class BCChBaseClient(metaclass=ABCMeta):
     credentials: Credentials | None = None
 
     base_url: str = "https://si3.bcentral.cl/SieteRestWS/SieteRestWS.ashx"
@@ -21,6 +30,19 @@ class BCChBaseClient:
     def __post_init__(self):
         if self.credentials is None:
             raise InvalidsCredentialsError("Client credentials must be provided.")
+
+    @abstractmethod
+    def get_series(
+        self,
+        time_serie: str,
+        first_date: str | date | datetime | None = None,
+        last_date: str | date | datetime | None = None,
+    ) -> WebServiceResponse:
+        raise NotImplementedError
+
+    @abstractmethod
+    def search_series(self, frequency: Frequency) -> WebServiceResponse:
+        raise NotImplementedError
 
     @property
     def transport(self):
