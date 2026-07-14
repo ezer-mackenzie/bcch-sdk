0d3259f47178beae09c0ae302e85526782532d7a
Eli-ezer Reuven Ramirez Ruiz <ramirez.ruiz.eliezer.reuven@gmail.com>
2026-07-14 15:37:01 -0400

    feat(api): expose public exceptions

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,30 @@
 from .sdk.async_sdk import BCChAsyncSDK
 from .sdk.sync_sdk import BCChSyncSDK
 
+from .exceptions import (
+    InvalidsCredentialsException,
+    InvalidDateException,
+    InvalidSeriesException,
+    InvalidFrequencyException,
+    WebServiceResponseException,
+    TransportException,
+    ResponseParseException,
+    BCChSDKBaseException,
+    InvalidConfigurationException,
+    InvalidCredentialsException,
+)
+
 __all__ = [
     "BCChSyncSDK",
     "BCChAsyncSDK",
+    "InvalidsCredentialsException",
+    "InvalidDateException",
+    "InvalidSeriesException",
+    "InvalidFrequencyException",
+    "WebServiceResponseException",
+    "TransportException",
+    "ResponseParseException",
+    "BCChSDKBaseException",
+    "InvalidConfigurationException",
+    "InvalidCredentialsException",
 ]
